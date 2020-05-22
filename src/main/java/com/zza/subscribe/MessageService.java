b913778b3da70664ff2adce48ea669482a3b253e
zhouzian <zza1998@foxmail.com>
2020-05-22 14:02:02 +0800

    初始化提交

--- src/main/java/com/zza/subscribe/MessageService.java
+++ src/main/java/com/zza/subscribe/MessageService.java
@@ -9,18 +9,18 @@ public class MessageService implements Service {
     @Override
     public boolean addSubscribe(final Observice observice) {
         observerMap.put(observice.getId(),observice);
-        System.out.println(String.format("用户Id %s 插入成功 ", observice.getId()));
+        System.out.println(String.format("用户Id %s 姓名 %s 插入成功 ", observice.getId(), observice.getName()));
         return true;
     }
 
     @Override
     public boolean removeSubscribe(final Observice observice) {
         if (observerMap.get(observice.getId()) == null){
-            System.out.println(String.format("用户Id %s 不存在 移除失败", observice.getId()));
+            System.out.println(String.format("用户Id %s 姓名 %s 不存在 移除失败", observice.getId(), observice.getName()));
             return false;
         }
         observerMap.remove(observice.getId());
-        System.out.println(String.format("用户Id %s 已经移除 ", observice.getId()));
+        System.out.println(String.format("用户Id %s 姓名 %s 已经移除 ", observice.getId(), observice.getName()));
         return true;
     }
 
@@ -28,5 +28,6 @@ public class MessageService implements Service {
     public synchronized void push(Object S) {
         if (null == S) return;
         observerMap.forEach((k,v)-> v.gotIt(S));
+        System.out.println("所有消息推送完毕");
     }
 }
